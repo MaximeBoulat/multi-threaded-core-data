MaximeBoulat/multi-threaded-core-data

commit fe3d5c40482ab9fe92f95a483b3d1a154cb3f689

Fix typos

CoreDataStack/CoreDataManager.m
@@ -95,9 +95,9 @@ typedef NS_ENUM(NSInteger, operationType){
     
     /* This is where the magic happens. 
      
-     All transactions are processed against a on-demand background child context which is instantiated for the lifetime and the transaction and then saved and discarded. The parent context is also saved (the main thread context) so that the changes propagate to the store.
+     All transactions are processed against a on-demand background child context which is instantiated for the lifetime of the transaction and then saved and discarded. The parent context is also saved (the main thread context) so that the changes propagate to the store.
      
-     All write/protected operations are 'barriered' through dependencies defined when the operation is added to the queue (writes will just be dependent on all pending operations, and reads will be dependents only on those pending operations in the queue which are writes).
+     All write/protected operations are 'barriered' through dependencies defined when the operation is added to the queue (writes will just be dependent on all pending operations, and reads will be dependent only on those pending operations in the queue which are writes).
      
      These conditions met, transactions, be them reads or writes, will never be faced with a context which is out of sync with the store. Since only writes can modify the store, and writes will only get served with a context once the previous write has completed and its changes are surfaced to the main thread context, from which the new work context is drawn.
  
@@ -138,17 +138,6 @@ typedef NS_ENUM(NSInteger, operationType){
 //        NSLog(@"operation of type: %@ source: %@ ENDING", protected? @"WRITE" : @"READ", source);
         
     }];
-    
-    if (protected)  // enforcing the dependencies
-    {
-        coreDataOperation.name = [NSString stringWithFormat:@"%ld", (long)OperationTypeWrite];
-    }
-    else
-    {
-        coreDataOperation.name = [NSString stringWithFormat:@"%ld", (long)OperationTypeRead];
-    }
-    
-    
     
     
     @synchronized(self)  //Synchronizing access to the queue! Multiple threads could be in there at the same time

CoreDataStack/CoreDataManager.h
@@ -14,12 +14,12 @@ typedef void(^CoreDataSerializedBlock)(NSManagedObjectContext *context);
 
 /* This Core data implementation offers two major advantages:
  * enforces thread safety when core data transactions are requested from the background
- * serialization of core data transactions using dependencies and a private queue to prevent multiple transactions to modify the same records concurrently
+ * serialization of core data transactions using dependencies and a private queue to prevent multiple transactions from modifying the same records concurrently
  */
 
 @interface CoreDataManager : NSObject
 
-@property (readonly, strong, nonatomic) NSManagedObjectContext *mainThreadContext; // This main thread context is leverages by the NSFetchedResultsControllers in the UI. It is not recommended to use it to write to Core Data, since doing so would bypass the serialization enforced in coordinateWriting/coordinateReading.
+@property (readonly, strong, nonatomic) NSManagedObjectContext *mainThreadContext; // This main thread context is leveraged by the NSFetchedResultsControllers in the UI. It is not recommended to use it to write to Core Data, since doing so would bypass the serialization enforced in coordinateWriting/coordinateReading.
 @property (nonatomic, strong) NSOperationQueue * coreDataQueue;
 
 +(CoreDataManager *) sharedCoreDataManager;

CoreDataStack/DataManager.m
@@ -132,7 +132,7 @@ typedef NS_ENUM(NSInteger, StressOperationType) {
     }
     
     
-    /* use private concurrent queue to bombard the CoreDataManager with database transactions. A while loop will randomely pick one of three transactions for every interation:
+    /* use private concurrent queue to bombard the CoreDataManager with database transactions. A while loop will randomly pick one of three transactions for every interation:
      - Delete a random range of records
      - Add 20 random records
      - Fetch and read the records 4 times

CoreDataStack/DetailViewController.m
@@ -30,7 +30,7 @@ NSFetchedResultsControllerDelegate
 
 -(void) loadGame: (Game*) game
 {
-    /* Configuring the FRC to the data source. Using KVO to prevent the UI, especially detail-type UIs which are often editable, from ever becoming des-synchronized with the underlying data. We dont want the user making edits on an object which no longer exists, and we dont want a user viewing a detail screen which is outdated.*/
+    /* Configuring the FRC to the data source. Using KVO to prevent the UI, especially detail-type UIs which are often editable, from ever becoming de-synchronized with the underlying data. We dont want the user making edits on an object which no longer exists, and we dont want a user viewing a detail screen which is outdated.*/
     
     NSFetchRequest * request = [[NSFetchRequest alloc]initWithEntityName:@"Game"];
     request.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES]];
@@ -82,7 +82,7 @@ NSFetchedResultsControllerDelegate
             [self.navigationResponder unwindNavigation:self];
         }
             break;
-        case NSFetchedResultsChangeUpdate:  // The data source has changes, update the screen not (not implemented in this sample)
+        case NSFetchedResultsChangeUpdate:  // The data source has changed, update the screen not (not implemented in this sample)
         {
             [self loadScreen];
         }

CoreDataStack/MainTableViewController.m
@@ -60,7 +60,7 @@ NavigationResponder
     [super viewDidLoad];
     
     
-    /* All the lists are synchronized with the data in the core data store through the use of NSFetchedResultsController configured with the approproate fetch request. There is no hard depency between user input and UI updates. The user input (delete row/add record) triggers some data activity (in the DataManager layer). The NSFetchedResultsControllers delegate methods in turn notify the UI of changes in the data layer.
+    /* All the lists are synchronized with the data in the core data store through the use of NSFetchedResultsController configured with the appropriate fetch request. There are no hard depencies between user input and UI updates. The user input (delete row/add record) triggers some data activity (in the DataManager layer). The NSFetchedResultsControllers delegate methods in turn notify the UI of changes in the data layer.
      The only exception is the Store list, which is populated with a traditional array of non-persistent objects
      */
     
@@ -279,7 +279,7 @@ NavigationResponder
      *the games list which will populate the split's detail pane with game details.
      *the store list which invokes its delegate to execute a data transaction and modal dismissal (see didPickGame:)
      The subsequent lists need a reference called relationshipObject on which to predicate their FetchedResultsController's request. For Platforms it is a Player, for Games it is a Platform. Those relationships are enforced in the data layer and in the database schema.
-     The currentMode variable provides for state under which to configure its appesarance and behavior*/
+     The currentMode variable provides for state under which to configure appearance and behavior*/
     
     switch (self.currentMode)
     {
@@ -338,7 +338,7 @@ NavigationResponder
 - (IBAction)didPressPlusButton:(UIBarButtonItem *)sender
 {
     
-    /* Pressing the '+' button from the Games list invokes the Store screen (where user can 'purchase' games to add them to their personal inventory. Pressing the '+' button anywhere else displays a alertview from a record can be inserted with a custom name.*/
+    /* Pressing the '+' button from the Games list invokes the Store screen (where user can 'purchase' games to add them to their personal inventory). Pressing the '+' button anywhere else displays a alertview from a record can be inserted with a custom name.*/
     
     if (self.currentMode == TableModeGame)
     {
@@ -459,7 +459,7 @@ NavigationResponder
 #pragma mark - NavigationResponder
 #pragma mark
 
-// THe detail pane is reporting to the master that the data source it's feeding from has been deleted. Letting the master make decisions about detail pane uypdates.
+// THe detail pane is reporting to the master that the data source it's feeding from has been deleted. Letting the master make decisions about detail pane updates.
 
 -(void) unwindNavigation: (UIViewController *) viewController
 { 

commit b160e8186f0a87f1d0073c843e382e629236a6d4

Fix regression introduced by fe3d5c4

CoreDataStack/CoreDataManager.m
@@ -139,6 +139,16 @@ typedef NS_ENUM(NSInteger, operationType){
         
     }];
     
+    if (protected)  // enforcing the dependencies
+    {
+        coreDataOperation.name = [NSString stringWithFormat:@"%ld", (long)OperationTypeWrite];
+    }
+    else
+    {
+        coreDataOperation.name = [NSString stringWithFormat:@"%ld", (long)OperationTypeRead];
+    }
+    
+     
     
     @synchronized(self)  //Synchronizing access to the queue! Multiple threads could be in there at the same time
     {